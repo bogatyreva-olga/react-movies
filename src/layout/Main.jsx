b75fb22908b7758a08b436f15c694830746dcac6
o.bogatyreva <bogatireva.o.v@gmail.com>
2023-01-18 01:41:04 +0300

    add env file

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import {Movies} from "../components/Movies";
 import {Preloader} from "../components/Preloader";
 import {Search} from "../components/ Search";
 
+const API_KEY = process.env.REACT_APP_API_KEY
+
 class Main extends React.Component {
     state = {
         movies: [],
@@ -10,14 +12,14 @@ class Main extends React.Component {
     }
 
     componentDidMount() {
-        fetch("http://www.omdbapi.com/?apikey=d39ad0bc&s=matrix")
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
     }
 
     searchMovies = (str, type = 'all') => {
         this.setState({ loading: true})
-        fetch(`http://www.omdbapi.com/?apikey=d39ad0bc&s=${str}${type !== 'all' ? `&type=${type}`: ''}`)
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}`: ''}`)
             .then(response => response.json())
             .then(data => this.setState({movies: data.Search, loading: false}))
 }
